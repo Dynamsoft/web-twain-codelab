d59f91beb7d76b957fa1240c3405e4e43e86cd64
Ellie-dynamsoft <Ellie@dynamsoft.com>
2025-01-21 14:25:20 +0800

    update

--- Resources/dynamsoft.webtwain.config_autoload.js
+++
@@ -1,7 +0,0 @@
-﻿
-var Dynamsoft = Dynamsoft || { DWT: {} };
-
-Dynamsoft.DWT.Containers = [{ ContainerId: 'dwtcontrolContainer',  Width: '100%',  Height:'100%'}];
-Dynamsoft.DWT.ProductKey = "DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9";
-Dynamsoft.DWT.ResourcesPath = 'https://cdn.jsdelivr.net/npm/dwt@latest/dist';
-Dynamsoft.DWT.AutoLoad = true;
