b39d00d8b9b325368484802ca5e8569a1023ca3d
Ellie-dynamsoft <Ellie@dynamsoft.com>
2025-01-21 14:25:47 +0800

    update

---
+++ js/config.js
@@ -0,0 +1,12 @@
+﻿
+var Dynamsoft = Dynamsoft || { DWT: {} };
+
+function initialization(Containers, bAutoLoad){
+	if(Containers)
+		Dynamsoft.DWT.Containers = Containers;
+	else
+		Dynamsoft.DWT.Containers = [];
+	Dynamsoft.DWT.ProductKey = "DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9";
+	Dynamsoft.DWT.ResourcesPath = 'https://cdn.jsdelivr.net/npm/dwt@latest/dist';
+	Dynamsoft.DWT.AutoLoad = bAutoLoad?true:false;
+}
